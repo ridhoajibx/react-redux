60840b5e051d91b841daf5f7fa6242d3af572533
ridhoajibx <ridhoajibx@gmail.com>
2020-11-10 05:20:19 +0700

    [ridho] react essetial-conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Card from './Card';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState('Alan Smith')
+    const [showCard, setShowCard] = useState(true)
+    const changeNameHandler = name => setName(name)
+    const changeInputHandler = event => setName(event.target.value)
+    const toggleShowCard = () => setShowCard(!showCard)
+    const buttonsMarkup = (
+        <div>
+            <button className="button button2">YES</button>
+            <button className="button button3">NO</button>
+        </div>
+    )
+
+    const cardsMarkup = showCard && <Card
+        avatar='https://s3.amazonaws.com/uifaces/faces/twitter/skkirilov/128.jpg'
+        name={name}
+        title='International Creative Administrator'
+        onChangeName={() => changeNameHandler('Michael Chan')}
+        onChangeInput={changeInputHandler}
+    >{buttonsMarkup}
+    </Card>
+    return (
+        <div className="App">
+            <button className="button" onClick={toggleShowCard}>Toggle show/hide</button>
+            {cardsMarkup}
+        </div>
+    );
 }
 
 export default App;

fc847aaeac48ea11c21e068a2859e76fe570cc6a
ridhoajibx <ridhoajibx@gmail.com>
2020-11-11 05:21:35 +0700

    [ridho] useReducer with useContext

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import Counter from './components/Counter'
-
+import ComponentA from './components/ComponentA'
+export const NameContext = React.createContext()
+export const ColorContext = React.createContext()
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <NameContext.Provider value={'Smith'}>
+        <ColorContext.Provider value={'red'}>
+          <ComponentA />
+        </ColorContext.Provider>
+      </NameContext.Provider>
     </div>
   );
 }
